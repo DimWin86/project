393e3539eb2922133243dd02239fe1db048cc4c7
Dmitrii <Dimwin86@gmail.com>
2024-01-05 12:02:30 +0500

    21 zanatie, logicheskie operatori

--- js/script.js
+++ js/script.js
@@ -60,7 +60,7 @@
 //     num++;
 // }
 
-let num = 50;
+// let num = 50;
 
 // do {
 //     console.log(num);
@@ -68,11 +68,57 @@ let num = 50;
 // }
 // while (num <= 55);
 
-for (let i = 1; i < 10; i++) {
-    if (i === 6) {
-        // break;
-        continue;
-    }
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6) {
+//         // break;
+//         continue;
+//     }
 
-    console.log(i);
-}
+//     console.log(i);
+// }
+
+// const hamburger = 5;
+// const fries = null;
+
+// if (hamburger && fries) {
+//     console.log('Я сыт!');
+// }
+
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 1;
+
+// console.log(hamburger === 3 && cola && fries);
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && 'dsafsdadsfsad');
+
+// if (hamburger === 3 && cola === 1 && fries ) {
+//     console.log('Все сыты!');
+// } else {
+//     console.log("Мы уходим");
+// }
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+//     console.log('Все довольны!');
+// } else {
+//     console.log("Мы уходим");
+// }
+
+// console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
+
+// let johnReport, 
+//     alexReport,
+//     samReport, 
+//     mariaReport = 'done';
+
+// console.log( johnReport || alexReport || samReport || mariaReport );
+
+// console.log(!1);
