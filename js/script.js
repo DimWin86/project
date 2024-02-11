2f4539d7b51b55e92965c9a600c0dee0816c4b68
Dmitrii <Dimwin86@gmail.com>
2024-02-11 16:12:10 +0500

    Function construction

--- js/script.js
+++ js/script.js
@@ -1725,4 +1725,36 @@
 
 
 
-// 74 //
+// 75 - Function construction //
+
+
+// const num = new Function(3);
+
+// console.log(num);
+
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function() {
+//         console.log(`Hello ${this.name}`)
+//     };
+// }
+
+// User.prototype.exit = function(name) {
+//     console.log(`Пользователь ${this.name} ушел`)
+// }
+
+// const ivan = new User('Ivan', 28);
+// const alex = new User('Alex', 20);
+
+// ivan.exit();
+
+// ivan.hello();
+// alex.hello();
+
+// console.log(ivan);
+// console.log(alex);
+
+
+// 76 - The context of callign this //
