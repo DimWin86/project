99329eb8d7d48443239e598360da537507b9f10e
Dmitrii <Dimwin86@gmail.com>
2024-02-11 17:19:57 +0500

    The context of callign this

--- js/script.js
+++ js/script.js
@@ -1757,4 +1757,88 @@
 // console.log(alex);
 
 
+
 // 76 - The context of callign this //
+
+// function showThis(a, b) {
+//     console.log(this);
+//     function sum() {
+//         console.log(this)
+//         return a + b;
+//     }
+
+/////////////////////////////////////////////////
+
+//     console.log(sum());
+// }
+// showThis(4, 5);
+
+// const obj = {
+//     a : 20,
+//     b : 15, 
+//     sum : function() {
+//         function shout() {
+//             console.log(this);
+//         }
+//         shout();
+//     }
+// }
+
+// obj.sum();
+
+/////////////////////////////////////////////////
+
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function() {
+//         console.log(`Hello ${this.name}`)
+//     };
+// }
+// let ivan = new User('Ivan', 23);
+
+
+// function sayName(surname) {
+//     console.log(this);
+//     console.log(this.name + surname);
+// }
+
+// const user = {
+//     name: 'John'
+// };
+
+// sayName.call(user, 'Smith');
+// sayName.apply(user, ['Smith']);
+
+// function count(num) {
+//     return this*num;
+// }
+
+// const double = count.bind(2);
+// console.log(double(3));
+// console.log(double(13));
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', e => {
+    e.preventDefault();
+    this.style.backgroundColor = 'red';
+});
+
+const obj = {
+    num: 5,
+    sayNumber: function() {
+        const say = () => {
+            console.log(this.num);
+        };
+
+        say();
+    }
+};
+
+obj.sayNumber();
+
+const double = (a, b) => a * 2;
+
+console.log(double(4));
