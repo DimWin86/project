8d3e62c4c05a1895b0f2e9526ae1d972886bb25b
Dmitrii <Dimwin86@gmail.com>
2024-02-15 22:04:24 +0500

    83 - Ajax and using server

--- js/script.js
+++ js/script.js
@@ -2006,3 +2006,47 @@
 
 
 // 83 - Ajax and using server//
+
+
+// const inputRub = document.querySelector('#rub'),
+//       inputUsd = document.querySelector('#usd');
+
+// inputRub.addEventListener('input', () => {
+//     const request = new XMLHttpRequest();
+
+//     request.open('GET', 'js/current.json');
+//     request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+//     request.send();
+
+//     request.addEventListener('load', () => {
+//         if (request.status === 200) {
+//             const data = JSON.parse(request.response);
+//             inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+//         } else {
+//             inputUsd.value = "Что-то пошло не так";
+//         }
+//     });
+
+// });
+
+// inputUsd.addEventListener('input', () => {
+//     const request = new XMLHttpRequest();
+
+//     request.open('GET', 'js/current.json');
+//     request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+//     request.send();
+
+//     request.addEventListener('load', () => {
+//         if (request.status === 200) {
+//             const data = JSON.parse(request.response);
+//             inputRub.value = (+inputUsd.value * data.current.usd).toFixed(2);
+//         } else {
+//             inputRub.value = "Что-то пошло не так";
+//         }
+//     });
+
+// });
+
+
+// 84 - implementation of the script for sending data to the server //
+
