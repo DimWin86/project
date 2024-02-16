b5d21ee370342ae33164ea38d81c8e31aca088c7
Dmitrii <Dimwin86@gmail.com>
2024-02-16 23:04:36 +0500

    84 - implementation of the script for sending data to the server

--- js/script.js
+++ js/script.js
@@ -2050,3 +2050,58 @@
 
 // 84 - implementation of the script for sending data to the server //
 
+// const forms = document.querySelectorAll('form'),
+//           message = {
+//             loading: 'Загрузка',
+//             success: 'Спасибо! Скоро мы с вами свяжемся', 
+//             fail: 'Что-то пошло не так...'
+//           };
+
+//     forms.forEach(item => {
+//         postData(item);
+//     });
+
+//     function postData(form) {
+//         form.addEventListener('submit', (e) => {
+//             e.preventDefault();
+
+//             const statusMessage = document.createElement('div');
+//             statusMessage.classList.add('status');
+//             statusMessage.textContent = message.loading;
+//             form.append(statusMessage);
+
+//             const request = new XMLHttpRequest();
+//             request.open('POST', 'server.php');
+            
+//             request.setRequestHeader('Content-type', 'application/json');
+//             const formData = new FormData(form);
+
+//             const object = {};
+//             formData.forEach(function(value, key){
+//                 object[key] = value;
+//             });
+            
+//             const json = JSON.stringify(object);
+
+//             request.send(json);
+
+//             request.addEventListener('load', () => {
+//                 if (request.status === 200) {
+//                     console.log(request.response);
+//                     statusMessage.textContent = message.success;
+//                     form.reset();
+//                     setTimeout(() => {
+//                         statusMessage.remove();
+//                     }, 2000);
+//                 } else {
+//                     statusMessage.textContent = message.fail;
+//                 }
+//             });
+
+
+//         });
+//     }
+
+
+
+// 85 - beautiful user notification //
