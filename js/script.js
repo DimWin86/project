ddbaddf380ed3732c1272515f3ff8c322c7384c4
Dmitrii <Dimwin86@gmail.com>
2024-02-26 21:53:38 +0500

    88 - Arra iteration methods

--- js/script.js
+++ js/script.js
@@ -2214,3 +2214,65 @@
 
 
 //  88 - Array Iteration Methods  //
+
+// filter 
+
+// const names = ['Ivan', 'Ann', 'Ksenia', 'Volondemart'];
+
+// const shortNames = names.filter(function(name) {
+//     return name.length < 5;
+// });
+
+// console.log(shortNames);
+
+
+// Map 
+
+// let answers = ['IvAn', 'AnnA', 'Hello'];
+
+// result = answers.map(item => item.toLowerCase());
+
+// console.log(result);
+
+
+// every/some
+
+// const some = [4, 5, 6];
+
+// console.log(some.some(item => typeof(item) === 'number'));
+
+// console.log(some.every(item => typeof(item) === 'number'));
+
+
+// Reduce
+
+// const arr = [4, 5, 1, 3, 2, 6];
+//                                             0           4
+//                                             4           5
+//                                             9           1
+//                                             10         3
+// const res = arr.reduce((sum, current) => sum + current);
+// console.log(res);
+
+// const arr = ['apple', 'pear', 'plum'];
+
+// const res = arr.reduce((sum, current) => `${sum}, ${current}`);
+// console.log(res);
+
+// Пример использования методов перебора массивов
+
+// const obj = {
+//     ivan: 'persone',
+//     ann : 'persone',
+//     dog: 'animal',
+//     cat: 'animal'
+// };
+
+// const newArr = Object.entries(obj)
+// .filter(item => item[1] === 'persone')
+// .map(item => item[0]);
+
+
+
+// console.log(newArr);
+
